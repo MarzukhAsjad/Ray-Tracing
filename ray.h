6c41d7e3ea260ec6ce1ef40eb330728559311601
MarzukhAsjad <marz.asjad00@gmail.com>
2023-05-07 01:07:21 +0800

    Update ray.h

--- ray.h
+++ ray.h
@@ -5,14 +5,17 @@
 
 class ray {
     public:
+        // constructors
         ray() {}
         ray(const point3& origin, const vec3& direction)
             : orig(origin), dir(direction)
         {}
 
+        // class attributes
         point3 origin() const  { return orig; }
         vec3 direction() const { return dir; }
 
+        // member function
         point3 at(double t) const {
             return orig + t*dir;
         }
