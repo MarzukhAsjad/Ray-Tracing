a6b1409ae1bef587d7ce46968ca21cd2c1854681
MarzukhAsjad <marz.asjad00@gmail.com>
2023-05-10 02:31:55 +0800

    Created background
    Created a background for the viewport (image1.ppm). This background will be included in the future updates.

--- ray_tracing.cpp
+++ ray_tracing.cpp
@@ -1,22 +1,41 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 #include <iostream>
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0); // smaller t corresponds to white and larger t corresponds to blue.
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0); // create a blend (smoothing lerp) between white and blue for the background
+}
 
 int main() {
 
     // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    const int image_width = 256;
-    const int image_height = 256;
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0; // distance between projection plane and projection point
+
+    auto origin = point3(0, 0, 0); // eye/camera
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.45); // hardcoding blue = 0.25
+            auto u = double(i) / (image_width-1); // normalised horizontal distance between left edge and current pixel
+            auto v = double(j) / (image_height-1); // normalised vertical distance between the bottom edge and current pixel
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin); // creating position vector for current pixel and then creating a ray object
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
